58bb8a305db8adcae5e42f8155d11fbec567d31f
Sheldon John Van Zyl <123169408+SheldonVZyl@users.noreply.github.com>
2024-03-27 09:10:39 +0200

    Add files via upload

---
+++ ls17pr1/Main.java
@@ -0,0 +1,14 @@
+package ls17pr1;
+
+/**
+ *
+ * @author MikeW
+ */
+public class Main {
+
+  public static void main(String[] args) {
+    
+      CalcTest.main(args);
+      
+  }
+}
---
+++ ls17pr3/Main.java
@@ -0,0 +1,14 @@
+package ls17pr3;
+
+/**
+ *
+ * @author MikeW
+ */
+public class Main {
+
+  public static void main(String[] args) {
+    
+      CalcTest.main(args);
+      
+  }
+}
