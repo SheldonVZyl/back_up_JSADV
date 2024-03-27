ebfcfea148061708d7991ffb92ab70ec2ae4bd05
Sheldon John Van Zyl <123169408+SheldonVZyl@users.noreply.github.com>
2024-03-27 09:19:07 +0200

    Add files via upload

--- Chapter_1/HelloApp2.java
+++ Chapter_1/HelloApp2.java
@@ -1,8 +1,12 @@
-package Book_1.Chapter_1;
+package Book_2.Chapter_1;
 
+// This application displays a hello message on →1
+// the console by creating an instance of the
+// Greeter class and then calling the Greeter
+// object's sayHello method.
 public class HelloApp2 {
-    public static void main(String[] args)
-    {
-        System.out.println("Hello, World 2!");
+    public static void main(String[] args) {
+        Greeter myGreeterObject = new Greeter();
+        myGreeterObject.sayHello();
     }
 }
